01d370dd3c443afb5bf17bb0c64599a12d7b36d4
TheClaireBear <daclaire65@gmail.com>
2018-04-26 19:07:00 -0500

    This is killing me inside

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-db_name = 'blogz'
-db_user = 'blogz'
-db_pass = 'password'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogs:abc123@localhost:8889/blogs'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = ''
+
+app.secret_key = 'abc123'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
